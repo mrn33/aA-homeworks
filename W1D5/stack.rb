dabffd4122fd6b81b2e55508c7c504145fc549fa
mrn33 <m.najafi@protonmail.com>
2018-06-21 19:17:56 -0700

    Update W1D5 stack.rb

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -1,2 +1,23 @@
 class Stack
-def init
+  def initialize
+    @mr_arr = []
+  end
+
+  def push(el)
+    mr_arr.push
+    el
+  end
+
+  def pop
+    mr_arr.pop
+  end
+
+  def peek
+    mr_arr.last
+  end
+
+  private
+
+  attr_reader :mr_arr
+
+end
